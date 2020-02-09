60c806dc3ab1585f7b8ef9e7dfa5981ffc7b3742
Harukine <jacobhtnguyen@gmail.com>
2020-02-09 01:05:28 -0500

    updated task-status-validation.pipe.ts to handle invalid statuses

--- src/tasks/pipes/task-status-validation.pipe.ts
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -1,9 +1,26 @@
-import { ArgumentMetadata, PipeTransform } from '@nestjs/common/interfaces/features/pipe-transform.interface';
+import { PipeTransform } from '@nestjs/common/interfaces/features/pipe-transform.interface';
+import { TaskStatus } from '../task.model';
+import { BadRequestException } from '@nestjs/common';
 
 export class TaskStatusValidationPipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata) {
-    console.log('value,', value);
-    console.log('metdata', metadata);
+  readonly allowedStatuses = [
+    TaskStatus.OPEN,
+    TaskStatus.IN_PROGRESS,
+    TaskStatus.DONE,
+  ];
+
+  transform(value: any) {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`"${value}" is an invalid status`);
+    }
+
     return value;
+  }
+
+  private isStatusValid(status: any) {
+    const idx = this.allowedStatuses.indexOf(status);
+    return idx !== -1;
   }
 }
