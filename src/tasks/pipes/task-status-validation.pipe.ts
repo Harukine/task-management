085418105a93409839568b69e9a2a5e2354b202b
Harukine <jacobhtnguyen@gmail.com>
2020-02-28 12:54:51 -0500

    updated TaskStatus to use entity

--- src/tasks/pipes/task-status-validation.pipe.ts
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -1,5 +1,5 @@
 import { PipeTransform } from '@nestjs/common/interfaces/features/pipe-transform.interface';
-import { TaskStatus } from '../task.model';
+import { TaskStatus } from '../task.entity';
 import { BadRequestException } from '@nestjs/common';
 
 export class TaskStatusValidationPipe implements PipeTransform {
